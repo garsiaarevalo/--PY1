bdc681169810ac03f5909b6351fd64c1c8dde1db
garsiaarevalo <118580910+garsiaarevalo@users.noreply.github.com>
2022-11-27 22:32:56 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task 9.py
@@ -0,0 +1,16 @@
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
+
+money_capital = 0  # количество денег, чтобы прожить 10 месяцев
+
+# TODO Оформить решение
+
+for i in range(0,10):
+    delta = spend - salary
+    money_capital = money_capital + delta
+    spend *= 1 + increase
+    salary *= 1
+
+print(round(money_capital))
